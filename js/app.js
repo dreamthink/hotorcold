f8d59e584feaaaeaf62086b4b1d1df5e91312fc8
dreamthinkstudios <emily.c.chen@gmail.com>
2016-11-01 12:56:30 -0400

    Initial commit - working JS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
+"use strict"
+var secretNumber,
+userGuess,
+feedback,
+userFeedback,
+gapNumber,
+pastGuesses = [],
+newButton,
+input,
+countElement,
+count = 0,
+guessList,
+validatedGuess,
+alreadyGuessed,
+guessHtml,
+max,
+min,
+form;
 
-$(document).ready(function(){
+$(document).ready(pageLoad);
+
+function pageLoad() {
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
@@ -12,66 +31,115 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-	/*--- Start new game upon page load and reload ---*/
-	var newGame = function() {
-		var secretNumber = Math.floor(Math.random()*(100)+1);
-		$("#feedback").text("Make your Guess!");
-		$("#count").text("0");
-	
-	/*--- Declares variable based on value entered in user guess box. ---*/
-	$("form").on("submit", function() {
-		var userGuess = $("#userGuess").val();
-		$("form")[0].reset();
+  	feedback = $("#feedback");
+  	form = $("form");
+  	input = form.find("#userGuess");
+  	newButton = $("a.new");
+  	countElement = $("#count");
+  	guessList = $("#guessList");
+  	startNewGame();
+}
 
-	/*--- Restricts user guess to numbers from 1 to 100. ---*/
-		if ((userGuess < 0) || (userGuess > 100)) {
-			$("#feedback").text("Please enter an integer from 1 to 100.");
-			return false;
-		};
+	function startNewGame() {
+		resetVariables();
+		generateSecretNumber();
+	}
 
-	/*--- Restricts user guess to integers. ---*/
-		if (userGuess % 1 != 0) {
-			$("#feedback").text("Please enter whole numbers only.");
-			return false;
-		};
-
-	/*--- Returns error message if user guess consist of spaces only. ---*/
-		if (($.trim(userGuess)).length === 0) {		
-			$("#feedback").text("Blanks are not allowed. Please enter something.");
-			return false;
-		};
-
-	/*--- Returns hot or cold feedback based on user guess ---*/
-		if (userGuess === secretNumber) {
-			$("#feedback").text("That's it! You guessed it!");
-		} else if (Math.abs(userGuess - secretNumber < 10)) {
-			$("#feedback").text("Very hot! Almost there.");
-			return false;
-		} else if (Math.abs(userGuess - secretNumber < 20)) {
-			$("#feedback").text("Very warm!");
-			return false;
-		} else if (Math.abs(userGuess - secretNumber < 30)) {
-			$("#feedback").text("Luke warm.");
-			return false;
-		} else if (Math.abs(userGuess - secretNumber < 40)) {
-			$("#feedback").text("Just cool.");
-			return false;
-		} else {
-			$("#feedback").text("Freezing cold.");
-			return false;
-		};
-
-	});
-};
-
-	newGame();
-
-	/*---- Start a new game upon clicking "NEW GAME" ---*/
 	$(".new").on("click", function() {
-		newGame();
+		startNewGame();
+	});
+	
+	function resetVariables() {
+		secretNumber = "";
+		userGuess = "";
+		pastGuesses = [];
+		count = 0;
+		userFeedback = "Make Your Guess!";
+		guessHtml = "";
+		render();
+	}
+
+	function generateSecretNumber(min, max) {
+		min = Math.ceil(1);
+		max = Math.floor(100);
+		secretNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+		console.log("Secret number is " + secretNumber);
+	}
+
+	/* Generate user guess after form is submitted */
+ 	$("form").submit(function(event) {	
+ 		event.preventDefault();
+ 		getUserGuess();
 	});
 
+ 	function getUserGuess() {
+		userGuess = input.val();
+		input.val("");
+		
+		if (checkGuess()) {
+			return ;
+		}
+	 	generateFeedback();
+	 	trackGuess();
+		guessCount();
+		render();
+	}
 
-});
+ 	function checkGuess() {
+ 		userFeedback = "";
+		if ((userGuess <= 0) || (userGuess > 100)) {
+			alert("Please enter a number between 1 and 100.");
+			return true;
+	 	} else if (userGuess % 1 !== 0) {
+ 			alert("Please enter integers only.");
+ 			// validatedGuess = false;
+ 			return true;
+ 		} else if (pastGuesses.indexOf(userGuess) !== -1) {
+ 			alert("You guessed this number already!");
+ 		} else {
+ 			validatedGuess = true;
+ 		}
+ 	}
 
+		function generateFeedback() {
+			userFeedback = "";
+			gapNumber = Math.abs(secretNumber - userGuess);
+			if (userGuess == secretNumber) {
+				userFeedback = "You won! Play again?";
+			} else if (gapNumber <= 10 && gapNumber >= 1) {
+				userFeedback = "Very hot!";
+			} else if (gapNumber <= 20 && gapNumber >= 11) {
+				userFeedback = "Hot!";
+			} else if (gapNumber <= 30 && gapNumber >= 21) {
+				userFeedback = "Warm";
+			} else if (gapNumber <= 49 && gapNumber >= 31) {
+				userFeedback = "Cold";
+			} else if (gapNumber >= 50) {
+				userFeedback = "Ice cold";
+			}
+		}
+	
+	function render() {
+		$("#guessList").html(guessHtml);
+		$("#count").html(count);
+		$("#feedback").html(userFeedback);	
+	}
+
+	function guessCount() {
+	count = pastGuesses.length;
+	}	
+
+	function trackGuess() {
+		if ((validatedGuess == true) && (pastGuesses.indexOf(userGuess) == -1)) {
+			pastGuesses.push(userGuess);
+		}
+
+		guessHtml = "";
+		if (pastGuesses[0].length) {
+			$.each(pastGuesses, function(guess, value) {
+				guessHtml += "<li>" + value + "</li>";
+			});
+		}
+
+	}
 
